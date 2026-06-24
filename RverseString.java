767cebf0c00737236d0b20541ca54a7f9ee57b7e
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-24 12:02:32 +0530

    Add files via upload

---
+++ RverseString.java
@@ -0,0 +1,13 @@
+package com.myproject;
+
+public class RverseString {
+public static void main(String[] args) {
+String str="Hello";
+String rev="";
+
+for(int i=str.length() -1;i>=0;i--) {	
+rev=rev+str.charAt(i);	
+}
+System.out.println(rev);
+}
+}
