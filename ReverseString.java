5b724038e6b2d68121d29236c4d277e3f5cd2418
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-24 12:20:14 +0530

    Enhance ReverseString to display original string
    Added output for original and reversed strings.

--- ReverseString.java
+++ ReverseString.java
@@ -8,6 +8,8 @@ String rev="";
 for(int i=str.length() -1;i>=0;i--) {	
 rev=rev+str.charAt(i);	
 }
-System.out.println(rev);
+System.out.println("Original String : " + str);
+System.out.println("Reversed String : " + rev);
+  
 }
 }
