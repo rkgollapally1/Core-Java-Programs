f9500a6f9fc13e39fb1a2049fb1310954ed86602
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-25 10:39:11 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package com.swarnalatha;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int n=10;
+	int first=0;
+	int second=1;
+	System.out.println("Fibanacci Series:");
+	for(int i=1; i<=n; i++) {
+		System.out.println(first+"  ");
+		
+		int next=first+second;
+		first=second;
+		second=next;
+		
+	}
+}
+}
