bc403e6c74d3ee5511c740b01ba8104195c2a265
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-26 15:50:34 +0530

    Add files via upload

---
+++ LargestOfThreeNumbers.java
@@ -0,0 +1,19 @@
+package com.swarnalatha;
+
+public class LargestOfThreeNumbers {
+public static void main(String[] args) {
+	
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	
+	if (a > b && a > c ) {
+		System.out.println("Largest Number is: " + a);
+	}else if (b > a && b > c) {
+		System.out.println("Largest Number is: " + b);
+	}else {
+		System.out.println("Largest Number is: " + c);
+		
+	}
+}
+}
