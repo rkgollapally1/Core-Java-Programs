a417527deecf2f14c6163632899606271f758c80
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-26 13:44:57 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+package com.swarnalatha;
+
+public class Armstrong {
+public static void main(String[] args) {
+	
+	int num=153;
+	int original =num;
+	int sum =0;
+	
+	while (num>0) {
+	int digit =num % 10;
+	sum = sum +(digit*digit*digit);
+	num = num /10;
+	}
+	if(original == sum) {
+		System.out.println("Armstrong Number");
+	}else {
+			System.out.println("Not Armstrong Number");
+		
+	}
+}
+}
