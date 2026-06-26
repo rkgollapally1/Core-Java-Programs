2dd04a4025810f281b0e0ab42c367f5a681a2774
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-26 15:07:49 +0530

    Add files via upload

---
+++ SwapTwoNumber.java
@@ -0,0 +1,25 @@
+package com.swarnalatha;
+
+public class SwapTwoNumber {
+public static void main(String[] args) {
+	
+	int a = 10;
+	int b =20;
+	int temp;
+	
+	System.out.println("Before Swapping");
+	System.out.println("a = " + a);
+	System.out.println("b = " + b);
+	
+	temp =a;
+	a =b;
+	b = temp;
+	
+	System.out.println("After Swaping");
+	System.out.println("a = " + a);
+	System.out.println("b = " + b);
+	
+	
+	
+}
+}
