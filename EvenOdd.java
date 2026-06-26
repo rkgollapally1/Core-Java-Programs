a24f19efdd9101f0e5a09ea2d4ca7e93294cae44
rkgollapally1 <67093224+rkgollapally1@users.noreply.github.com>
2026-06-26 14:51:41 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+package com.swarnalatha;
+
+public class EvenOdd {
+ public static void main(String[] args) {
+	
+	 int num =10;
+	 
+	 if (num % 2 ==0) {
+		 System.out.println("Even Number");
+		 
+	 }else {
+		 System.out.println("Odd Number");
+	 }
+}
+}
